a0bee78409fcffc9ec8114e71bcf19d0eadff747
Vengance098 <rishavshaw30@gmail.com>
2026-07-18 18:22:33 +0530

    oractice

---
+++ chapter 3/popping_position.py
@@ -0,0 +1,3 @@
+motorcycle=['honda','yamaha','suzuki']
+first_owned=motorcycle.pop(0)
+print(f"the first the car i knowned is {first_owned.title()}.")
---
+++ chapter 3/popremove.py
@@ -0,0 +1,6 @@
+motorcycles=['honda','yamaha','suzuki']
+print(motorcycles)
+popped_motorcycle=motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
+print(f"the last motorcycle I owned was a {popped_motorcycle}.")
