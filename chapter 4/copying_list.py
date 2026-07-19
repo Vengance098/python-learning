d840d044f18bd31efdf81d57f0808a7cb54194d3
Vengance098 <rishavshaw30@gmail.com>
2026-07-19 16:59:38 +0530

    practice

---
+++ chapter 4/copying_list.py
@@ -0,0 +1,8 @@
+my_foods=['pizza','falafel','carrot cake']
+friend_foods=my_foods[:]
+
+print("my fav food are:")
+print(my_foods)
+
+print("my friend fav food are:")
+print(friend_foods)
---
+++ chapter 4/writing_over_tupple.py
@@ -0,0 +1,9 @@
+dimensions=(200,50)
+print("original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions=(400,100)
+print("modified dimensions:")
+for dimension in dimensions:
+    print(dimension)    
